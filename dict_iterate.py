61885e4b3bc8d278da2950b6fbb3787cb243b722
Shivam Mahajan <shivammahajan1908@gmail.com>
2024-08-04 19:29:16 +0530

    dictionarymethods

---
+++ dict_iterate.py
@@ -0,0 +1,11 @@
+instructor = {
+    "name":  "Colt",
+    "own_dog":True,
+    "is_hilarious": False,
+    44: "my favoritenumber"
+}
+
+for key,value in instructor.items():
+    print(key,value)
+
+print( instructor.items())
