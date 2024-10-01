a88d360c88d3ea8c5c0f80fa3e1bb5787020ee51
Shivam Mahajan <shivammahajan1908@gmail.com>
2024-10-01 23:20:54 +0530

    File IO using with clause

---
+++ write_file_io.py
@@ -0,0 +1,3 @@
+with open("data/output/sample_output.txt","w") as file:
+    file.write("This is the sample output file \n")
+    file.write("This directory contain the program output files if any")
