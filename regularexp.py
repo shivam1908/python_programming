c30500239425a7fdb24ba2c49c1e51827bde7c88
Shivam Mahajan <shivammahajan1908@gmail.com>
2024-11-02 18:57:23 +0530

    regular expression

---
+++ regularexp.py
@@ -0,0 +1,10 @@
+#import regex module
+import  re
+
+#defint the pattern
+contact = re.compile(r'(\+91)*\d{10}')
+print(f"type of contact pattern is {type(contact)}")
+
+# searh for mobile number in the string  -- returting the match object
+contact_det = contact.search('My contact no is +919814447599')
+print(f"the contact no is : {contact_det.group()}")
